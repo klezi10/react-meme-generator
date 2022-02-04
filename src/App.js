c6a6fea48a87e7698ce964dd5d2cd91a6047e2b6
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-04 09:35:47 +0700

    created Meme component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Header from './Components/Header';
+import Meme from './Components/Meme';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/Components/Meme.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Meme() {
+  return (
+    <div className="Meme">
+      <h2>Hello from Meme</h2>
+    </div>
+  );
+}
