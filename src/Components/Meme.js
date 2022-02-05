16606fc044fdce1ac0043f84e61e71d26d35bb67
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-05 17:38:43 +0700

    set the meme img state to random image

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
-  function handleMeme() {
+  const [memeImage, setMemeImage] = useState('');
+
+  function getMemeImg() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const randomImg = memesArray[randomNumber].url;
-    console.log(randomImg);
+    setMemeImage(randomImg);
   }
 
   return (
@@ -14,10 +16,11 @@ export default function Meme() {
       <div className="form">
         <input type="text" placeholder="Top text" />
         <input type="text" placeholder="Bottom text" />
-        <button className="btn" onClick={handleMeme}>
+        <button className="btn" onClick={getMemeImg}>
           Get a new meme image 🖼️
         </button>
       </div>
+      <img src={memeImage} alt="random meme" className="memeImg" />
     </div>
   );
 }
