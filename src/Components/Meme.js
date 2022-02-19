162e808202eadee98f3e5fdd3e5b48514cb33eef
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-19 12:46:03 +0700

    set up form to add text to meme img

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -19,19 +19,40 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <div className="Meme">
       <div className="form">
-        <input type="text" placeholder="Top text" />
-        <input type="text" placeholder="Bottom text" />
+        <input
+          type="text"
+          placeholder="Top text"
+          value={meme.topText}
+          onChange={handleChange}
+          name="topText"
+        />
+
+        <input
+          type="text"
+          placeholder="Bottom text"
+          value={meme.bottomText}
+          onChange={handleChange}
+          name="bottomText"
+        />
         <button className="btn" onClick={getMemeImg}>
           Get a new meme image 🖼️
         </button>
       </div>
       <div className="meme-section">
         <img src={meme.randomImage} alt="random meme" className="memeImg" />
-        <h2 className="meme-text top-text">One does not simply</h2>
-        <h2 className="meme-text bottom-text">Walk into mordor</h2>
+        <h2 className="meme-text top-text">{meme.topText}</h2>
+        <h2 className="meme-text bottom-text">{meme.bottomText}</h2>
       </div>
     </div>
   );
